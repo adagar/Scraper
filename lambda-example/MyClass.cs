23396e3d3b34bad921c1dfa5d0be5730a52df995
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2018-07-28 10:24:32 -0400

    Trying lambda version

---
+++ lambda-example/MyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializerAttribute(
+    typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace lambda_example
+{
+    public class MyClass
+    {
+        public object MyFunction()
+        {
+            return new
+            {
+                message = "Hello from Lambda!",
+                time = DateTime.UtcNow
+            };
+        }
+    }
+}
---
+++ selenium_ex/WebDriverScraping/WebDriverScraping/Scraper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializerAttribute (typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace WebDriverScraping
+{
+    class Scraper
+    {
+        public object TestFunction()
+        {
+            return new
+            {
+                message = "Hello from Lambda!",
+                time = DateTime.Now
+            };
+        }
+    }
+}
