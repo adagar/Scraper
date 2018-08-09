4a99e0a5ca858dce21f8272d51d7cda62eff7b81
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2018-08-09 18:32:53 -0400

    First unit test - successfully scrapes

--- scraper_mvc/ScraperMVC.Tests/ScraperTests.cs
+++ scraper_mvc/ScraperMVC.Tests/ScraperTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Scraper;
 using NUnit.Framework;
+using System.Threading.Tasks;
+
 
 namespace ScraperMVC.Tests
 {
@@ -8,5 +10,13 @@ namespace ScraperMVC.Tests
     public class ScraperTests
     {
         [Test]
+        public void ScraperShouldReturn10Items()
+        {
+            var scraper = new Scraper.Services.ScrapeService();
+
+            Task<Scraper.Models.StockItem[]> output = scraper.ScrapePortfolioAsync();
+            output.Wait();
+            Assert.AreEqual(output.Result.Length, 10);
+        }
     }
 }
