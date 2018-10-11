f7063cf0d3a69104a7075e229a40c8a13292c817
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2018-10-11 19:53:49 -0400

    Created a view model, snapshot model, and snapshot controller for new snapshot view

---
+++ scraper_mvc/Scraper/Controllers/SnapshotController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Scraper.Controllers
+{
+    public class SnapshotController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Get snapshots from database
+
+
+            //Put items into a model
+
+
+            //Render view using the model
+        }
+    }
+}
